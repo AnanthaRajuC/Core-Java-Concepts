0ca41d9200b541a014da88b869d1ac435b30c243
anantharaju <anantharaju>
2017-01-24 15:30:30 +0530

    Iteration Statements added

---
+++ IterationStatements/src/io/github/anantharajuc/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package io.github.anantharajuc;
+
+public class DoWhileLoop 
+{
+	public static void main(String[] args) 
+	{
+		int i = 25;  
+		
+        do  
+        {  
+            System.out.println( "value of i = " + i );  
+            i++;  
+        }  
+        while ( i < 5);  	
+	}
+}
---
+++ IterationStatements/src/io/github/anantharajuc/ForEachLoop.java
@@ -0,0 +1,15 @@
+package io.github.anantharajuc;
+
+public class ForEachLoop 
+{
+
+	public static void main(String[] args) 
+	{
+		int[] i = {23, 7, 64, 20, 3}; 
+		
+		for ( int j : i )  
+        {  
+            System.out.println( "value :: " + j );  
+        }  	
+	}
+}
