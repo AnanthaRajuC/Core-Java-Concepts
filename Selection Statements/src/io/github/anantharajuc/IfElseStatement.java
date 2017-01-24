88dc30612547a09a2faf9930217b60b681fe0cd1
anantharaju <anantharaju>
2017-01-24 15:13:07 +0530

    Selection Statements Added

---
+++ Selection Statements/src/io/github/anantharajuc/IfElseStatement.java
@@ -0,0 +1,19 @@
+package io.github.anantharajuc;
+
+public class IfElseStatement 
+{
+	public static void main(String[] args) 
+	{
+		int age = 14;
+		
+		if (age > 18) 
+		{
+			System.out.println("Eligible to vote!");			
+		} 
+		else 
+		{
+			System.out.println("Not Eligible to vote!");	
+
+		}
+	}
+}
